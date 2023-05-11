ac6e4e87dcb8d4512896c5e87f83eff7765752bf
Перлыгин Артемий <perelygin.ad@edu.spbstu.ru>
2023-05-12 01:04:55 +0300

    module8-task1

---
+++ js/form.js
@@ -0,0 +1,30 @@
+const form = document.querySelector('.img-upload__form');
+
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__text',
+  successClass: 'form--valid',
+  errorClass: 'form--invalid',
+  errorTextParent: 'img-upload__text',
+  errorTextTag: 'span',
+  errorTextClass: 'form__error'
+});
+
+const TextField = {
+  minLength: 20,
+  maxLength: 140,
+}
+
+function validateComment(comment) {
+  if (comment.length >= TextField.minLength && comment.length <= TextField.maxLength) {
+    document.querySelector('.img-upload__submit').disabled = false;
+    return true;
+  }
+  document.querySelector('.img-upload__submit').disabled = true;
+  return false;
+}
+
+pristine.addValidator(
+  form.querySelector('.text__description'),
+  validateComment,
+  `Комментарий дложен быть от ${TextField.minLength} до ${TextField.maxLength} символов.`
+);
--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 import { getRandInt, lengthCheck } from './util.js';
 import './data.js';
 import './draw.js';
+import './bigPicture.js';
+import './form.js';
 
 getRandInt(0, 200);
 lengthCheck('', 12);
